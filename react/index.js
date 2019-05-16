1ab849b16119dcc773d52145bca2dee183b48712
jcaceitunowebimpacto <jcaceituno@webimpacto.es>
2019-05-16 13:02:47 +0200

    Commit 16-05-2019

--- react/index.js
+++ react/index.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { ExtensionPoint, useRuntime } from 'vtex.render-runtime'
-
+import styles from './styles.css';
 
 
 export default class HeaderBikinis extends Component {
@@ -39,11 +39,17 @@ export default class HeaderBikinis extends Component {
         const {image}=this.props;
 
         return (
-            <div className="flex justify-center">
-                <img src={image}/>
+            <div>
+                <ExtensionPoint id="topbar"/>
+                <div className={styles.parent}>
+                    <img src={image} className={styles.child}/>
 
-                <ExtensionPoint id="topbar" />
+                    <div className={styles.child}>
+                        <ExtensionPoint id="minicartjc"/>
+                    </div>
+                </div>
 
+                <ExtensionPoint id="menujc" className=""/>
             </div>
         )
     }
